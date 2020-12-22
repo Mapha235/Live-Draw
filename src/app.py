542cc1301f24112b621aa84ad8041bcfd3e3de1b
Martin <phammartin235@gmail.com>
2020-12-22 11:43:34 +0100

    Added eraser.

--- src/app.py
+++ src/app.py
@@ -1,13 +1,17 @@
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import QPoint, QRect, QSize, Qt
 from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QApplication, QPushButton
-from PyQt5.QtGui import QPixmap, QPainter, QPen
+from PyQt5.QtGui import QColor, QImage, QPainter, QPen, QPixmap
 
 from tools import Tools
+
 
 class Window(QMainWindow):
 
     def __init__(self):
         super(Window, self).__init__()
+
+        self.is_eraser = False
+        self.eraser_rect = QRect(QPoint(), QSize())
 
         self.pen_color = Qt.red
 
@@ -22,6 +26,9 @@ class Window(QMainWindow):
         self.setGeometry(0, 0, self.image.width(), self.image.height())
         self.showFullScreen()
 
+        self.canvas = self.image.copy(
+            QRect(0, 0, self.image.width(), self.image.height()))
+
         self.setCentralWidget(self.main_widget)
 
         self.tools = Tools()
@@ -29,24 +36,16 @@ class Window(QMainWindow):
         self.tools.show()
         self.tools.blue_btn.clicked.connect(lambda: self.setColor(Qt.blue))
         self.tools.red_btn.clicked.connect(lambda: self.setColor(Qt.red))
+        self.tools.eraser_btn.clicked.connect(self.toggle)
 
     def initUI(self):
         self.main_layout = QGridLayout()
-        # self.main_layout.addWidget(self.btn, 0, 0, 1, 1)
-        # self.main_layout.addWidget(self.tools, 0, 0, 1, 1)
-        # self.main_layout.addWidget(self.image, 1, 0, 1, 1)
-        
-        # self.bg = QImage()
-        # scaled_bg = self.bg.scaled(QSize(self.my_width, self.my_height))
-        # palette = QPalette()
-        # palette.setBrush(QPalette.Background, QBrush(scaled_bg))
-        # self.setPalette(palette)
 
         self.main_widget.setLayout(self.main_layout)
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.drawPixmap(self.rect(), self.image)
+        painter.drawPixmap(self.rect(), self.canvas)
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -55,9 +54,18 @@ class Window(QMainWindow):
 
     def mouseMoveEvent(self, event):
         if event.buttons() and Qt.LeftButton and self.drawing:
-            painter = QPainter(self.image)
-            painter.setPen(QPen(self.pen_color, 3, Qt.SolidLine))
-            painter.drawLine(self.lastPoint, event.pos())
+            painter = QPainter(self.canvas)
+            if self.is_eraser:
+                r = QRect(QPoint(), 20*QSize(1, 1))
+                r.moveCenter(event.pos())
+                px = self.image.copy(r)
+                painter.save()
+                painter.eraseRect(r)
+                painter.restore()
+                painter.drawPixmap(r, px)
+            else:
+                painter.setPen(QPen(self.pen_color, 3, Qt.SolidLine))
+                painter.drawLine(self.lastPoint, event.pos())
             self.lastPoint = event.pos()
             self.update()
 
@@ -67,3 +75,7 @@ class Window(QMainWindow):
 
     def setColor(self, color):
         self.pen_color = color
+        self.is_eraser = False
+
+    def toggle(self):
+        self.is_eraser = not self.is_eraser
